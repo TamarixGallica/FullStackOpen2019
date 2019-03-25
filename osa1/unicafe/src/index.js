def4f2167e162690f0aeb32c3b1fd2537a40167d
Jani Havukainen <github@posti.titityy.net>
2019-03-25 18:03:57 +0000

    Tehtävä 1.6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({message}) => <h1>{message}</h1>
+const Stat = ({label, count}) => <p>{label} {count}</p>
+
+const App = () => {
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
+    
+    return (
+    <div>
+        <Header message={"anna palautetta"} />
+        <button onClick={handleGoodClick}>hyvä</button>
+        <button onClick={handleNeutralClick}>neutraali</button>
+        <button onClick={handleBadClick}>huono</button>
+        <Header message={"statistiikka"} />
+        <Stat label="hyvä" count={good} />
+        <Stat label="neutraali" count={neutral} />
+        <Stat label="huono" count={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
