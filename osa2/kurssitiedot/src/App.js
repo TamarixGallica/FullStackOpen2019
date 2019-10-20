e1e688e8403917087a7ad6bfdbba399bdb64565e
Jani Havukainen <jani.havukainen@aten.fi>
2019-10-20 13:31:46 +0300

    Tehtävät 2.4 ja 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,28 +1,57 @@
 import React from 'react'
 import Course from './Course'
+import PageHeader from './PageHeader'
 
 const App = () => {
-    const course = {
-      name: 'Half Stack -sovelluskehitys',
+  const courses = [
+    {
+      name: 'Half Stack application development',
       parts: [
         {
-            name: 'Reactin perusteet',
-            exercises: 10
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
         },
         {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
         },
         {
-          name: 'Komponenttien tila',
-          exercises: 14
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
         }
       ]
     }
+  ]
   
     return (
       <div>
-          <Course course={course} />
+          <PageHeader title="Web development curriculum" />
+          {courses.map(course =>
+            <Course course={course} />
+          )}
       </div>
     )
   }
---
+++ osa2/kurssitiedot/src/PageHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PageHeader = (props) => {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
+
+
+export default PageHeader
