7a14baddbeb55626b23bd7e950987908111cc7d9
Jani Havukainen <jani.havukainen@aten.fi>
2019-10-20 13:20:00 +0300

    Korjattu tehtävät 1.13 ja 1.14

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -11,63 +11,50 @@ const RandomNumber = ({length}) => {
     return Math.floor(Math.random() * length)
 }
 
-const HighestVotedAnecdote = (anecdotes) => {
-  const votes = anecdotes.map(x => x.votes);
-  const HighestVoted = Math.max.apply(null, votes);
-  return anecdotes[HighestVoted];
+const HighestVotedAnecdote = (votes) => {
+  const MostVotes = Math.max.apply(null, votes);
+  return votes.indexOf(MostVotes);
 }
 
-const Anecdote = ({anecdote}) => {
+const Vote = (votes, index) => {
+  const copy = [...votes];
+  copy[index] += 1;
+  return copy;
+}
+
+const Anecdote = ({anecdote, votes}) => {
 
   return (
     <p>
-      {anecdote.anecdote}<br/>
-      has {anecdote.votes} votes
+      {anecdote}<br/>
+      has {votes} votes
     </p>
   )
 }
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const voteSelected = useState(0)[1];
+  const [votes, voteSelected] = useState([0,0,0,0,0,0]);
 
   return (
     <div>
       <h1>Anecdote of the day</h1>
-      <Anecdote anecdote={props.anecdotes[selected]} />
-      <Button clickEvent={() => voteSelected(props.anecdotes[selected].votes += 1)} label="vote" />
+      <Anecdote anecdote={props.anecdotes[selected]} votes={votes[selected]} />
+      <Button clickEvent={() => voteSelected(Vote(votes, selected))} label="vote" />
       <Button clickEvent={() => setSelected(RandomNumber(anecdotes))} label="next anecdote" />
       <h1>Anecdote with the most votes</h1>
-      <Anecdote anecdote={HighestVotedAnecdote(props.anecdotes)} />
+      <Anecdote anecdote={props.anecdotes[HighestVotedAnecdote(votes)]} votes={Math.max.apply(null, votes)} />
     </div>
   )
 }
 
 const anecdotes = [
-  {
-    anecdote: 'If it hurts, do it more often',
-    votes: 0
-  },
-  {
-    anecdote: 'Adding manpower to a late software project makes it later!',
-    votes: 0
-  },
-  {
-    anecdote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    votes: 0
-  },
-  {
-    anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    votes: 0
-  },
-  {
-    anecdote: 'Premature optimization is the root of all evil.',
-    votes: 0
-  },
-  {
-    anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    votes: 0
-  }
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
