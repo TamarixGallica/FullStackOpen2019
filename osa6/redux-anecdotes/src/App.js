8ae8f93dd7b02ecb39a5d0eb5d501a460b5009c9
Jani Havukainen <jani.havukainen@alten.fi>
2020-01-05 17:58:17 +0200

    Tehtävä 6.8

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
-import _ from 'lodash'
+import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = ({ store }) => {
-  const anecdotes = _.orderBy(store.getState(), ['votes'], ['desc'])
-
-  const vote = (id) => {
-    store.dispatch({
-      type: 'vote',
-      data: {
-        id
-      }
-    })
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={store} />
       <AnecdoteForm store={store} />
     </div>
   )
---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import _ from 'lodash'
+
+const AnecdoteList = ({ store }) => {
+
+    const anecdotes = _.orderBy(store.getState(), ['votes'], ['desc'])
+
+    const vote = (id) => {
+        store.dispatch({
+            type: 'vote',
+            data: {
+                id
+            }
+        })
+    }
+
+
+    return (
+        <>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default AnecdoteList
